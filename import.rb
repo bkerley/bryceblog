567e0d00c297351c10d68db9e84207b9e484f74b
Bryce Kerley <bkerley@brycekerley.net>
2019-03-10 16:48:46 -0400

    first import pass

--- import.rb
+++ import.rb
@@ -27,35 +27,36 @@ posts_xml.css("posts").children.each do |post|
               strip
 
     title = date.getlocal.to_s if title.empty?
+  end
 
-    kind = nil
-    body = nil
+  kind = nil
+  body = nil
 
-    if (body_el = post.css('regular-body')).length > 0
-      kind = 'regular'
-      body = CGI.unescapeHTML post.css('regular-body').text
-    elsif (convo_el = post.css('conversation')).length > 0
-      kind = 'conversation'
-      convo = convo_el.css('line').map do |line|
-        "<dt>#{CGI.unescapeHTML line['label']}</dt>\n<dd>#{CGI.unescapeHTML line.text}</dd>"
-      end.join("\n")
-      body = "<dl>\n#{convo}\n</dl>"
-    elsif (photo_url_els = post.css('photo-url')).length > 0
-      kind = 'photo'
-      media = Dir.glob("./ingest/#{DIR}/media/#{id}*")
-      body = media.map do |m|
-        m.inspect
-      end
-    elsif (quote_el = post.css('quote-text')).length > 0
-      kind = 'quote'
-      quote_source_el = post.css('quote-source')
-      body = "<blockquote>#{CGI.unescapeHTML quote_el.text}</blockquote>\n<cite>#{CGI.unescapeHTML quote_source_el.text}</cite>"
-    else
-      kind = 'unknown'
-      body = "<pre>#{CGI.escapeHTML post.to_xml}</pre>"
+  if (body_el = post.css('regular-body')).length > 0
+    kind = 'regular'
+    body = CGI.unescapeHTML post.css('regular-body').text
+  elsif (convo_el = post.css('conversation')).length > 0
+    kind = 'conversation'
+    convo = convo_el.css('line').map do |line|
+      "<dt>#{CGI.unescapeHTML line['label']}</dt>\n<dd>#{CGI.unescapeHTML line.text}</dd>"
+    end.join("\n")
+    body = "<dl>\n#{convo}\n</dl>"
+  elsif (photo_url_els = post.css('photo-url')).length > 0
+    kind = 'photo'
+    media = Dir.glob("./ingest/#{DIR}/media/#{id}*")
+    body = media.map do |m|
+      m.inspect
     end
+  elsif (quote_el = post.css('quote-text')).length > 0
+    kind = 'quote'
+    quote_source_el = post.css('quote-source')
+    body = "<blockquote>#{CGI.unescapeHTML quote_el.text}</blockquote>\n<cite>#{CGI.unescapeHTML quote_source_el.text}</cite>"
+  else
+    kind = 'unknown'
+    body = "<pre>#{CGI.escapeHTML post.to_xml}</pre>"
+  end
 
-    fm = <<<EOM
+  fm = <<EOM
 ---
 layout: #{kind}
 title: #{title.inspect}
@@ -63,12 +64,13 @@ date: #{date.iso8601}
 ---
 EOM
 
-    filename = "./_posts/#{date.strftime('%Y-%m-%d)}.html"
+  filename = "./_posts/#{date.strftime('%Y-%m-%d')}-#{title.dasherize}.html"
 
-    File.open(filename, 'w') do |f|
-      f.write fm
-      f.write body
-    end
+  File.open(filename, 'w') do |f|
+    f.write fm
+    f.write "{% raw %}"
+    f.write body
+    f.write "{% endraw %}"
   end
 
 end
